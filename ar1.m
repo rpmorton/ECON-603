20a0e4b7dabeeff2afbfa695af05126201ac54c2
Russell Morton <russell.morton@gmail.com>
2018-01-23 21:38:13 -0500

    Set up ECON 607 folder

---
+++ ar1.m
@@ -0,0 +1,22 @@
+%AR 1 Function for use in PS1 Q1
+
+function ar1vec = ar1(f_alpha,f_rho,f_sigma_2,f_T,f_y0);
+pre_ar1vec = [1:f_T]';
+
+for i=1:f_T ;
+    if i == 1
+        pre_ar1vec(i,1) = f_y0;
+        f_ylag = f_y0;
+       % yalt = y0;
+    end;
+    if i > 1
+        randnorm = randn(1,1)*f_sigma_2^(1/2);
+        pre_ar1vec(i,1) = f_alpha + f_rho * f_ylag + randnorm;
+        f_ylag = pre_ar1vec(i,1);
+       % yalt = [yalt', ylag]';
+    end;
+end;
+
+ar1vec = pre_ar1vec;
+
+%end;
